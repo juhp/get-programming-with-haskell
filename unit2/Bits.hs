bfba7e998a63dc1968ad54968b6c821c0f673fe9
Jens Petersen <juhpetersen@gmail.com>
2021-09-14 19:17:04 +0800

    Bits: use Word to avoid negative Int's
    They do not terminate

--- unit2/Bits.hs
+++ unit2/Bits.hs
@@ -1,35 +1,38 @@
 module Bits where
 
+import Data.Word
+
 type Bits = [Bool]
 
-intToBits' :: Int -> Bits
-intToBits' 0 = [False]
-intToBits' 1 = [True]
-intToBits' n = if (remainder == 0)
-               then False : (intToBits' nextVal)
-               else True : (intToBits' nextVal)
- where remainder = n `mod` 2
+wordToBits :: Word -> Bits
+wordToBits n = leadingFalses ++ reversedBits
+  where
+    reversedBits = reverse  (intToBits' n)
+    missingBits = maxBits - (length reversedBits)
+    leadingFalses = take missingBits (cycle [False])
+
+    maxBits :: Int
+    maxBits = length (intToBits' maxBound)
+
+    intToBits' :: Word -> Bits
+    intToBits' 0 = [False]
+    intToBits' 1 = [True]
+    intToBits' n = if (remainder == 0)
+                   then False : (intToBits' nextVal)
+                   else True : (intToBits' nextVal)
+     where
+       remainder = n `mod` 2
        nextVal = n `div` 2
 
-maxBits :: Int
-maxBits = length (intToBits' maxBound)
-
-intToBits :: Int -> Bits
-intToBits n = leadingFalses ++ reversedBits
-   where reversedBits = reverse  (intToBits' n)
-         missingBits = maxBits - (length reversedBits)
-         leadingFalses = take missingBits (cycle [False])
-
 charToBits :: Char -> Bits
-charToBits char = intToBits (fromEnum char)
+charToBits char = wordToBits (toEnum $ fromEnum char)
 
-bitsToInt :: Bits -> Int
-bitsToInt bits = sum (map (\x -> 2^(snd x)) trueLocations)
- where size = (length bits)
-       indices = [size-1,size-2 .. 0]
-       trueLocations = filter (\x ->
-                                ((fst x) == True))
-                       (zip bits indices)
+bitsToWord :: Bits -> Word
+bitsToWord bits = sum (map (\x -> 2^(snd x)) trueLocations)
+  where
+    size = (length bits)
+    indices = [size-1,size-2 .. 0]
+    trueLocations = filter (\x -> (fst x == True)) (zip bits indices)
 
 bitsToChar :: Bits -> Char
-bitsToChar bits = toEnum (bitsToInt bits)
+bitsToChar = toEnum . fromEnum . bitsToWord
