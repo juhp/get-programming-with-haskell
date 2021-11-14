a19d0adaad63fc2d50139d689c452b0ddc9e15bf
Jens Petersen <juhpetersen@gmail.com>
2021-11-14 21:49:17 +0800

    candidates: Enum redundant

--- unit5/candidates.hs
+++ unit5/candidates.hs
@@ -1,8 +1,8 @@
 data Grade = F | D | C | B | A
-  deriving (Eq, Ord, Enum, Show, Read)
+  deriving (Eq, Ord, Show, Read)
 
 data Degree = HS | BA | MS | PhD
-  deriving (Eq, Ord, Enum, Show, Read)
+  deriving (Eq, Ord, Show, Read)
 
 data Candidate = Candidate
   {candidateId :: Int
