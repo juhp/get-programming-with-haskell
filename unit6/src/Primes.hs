3dadb3102ada2a10171ce049c01f14b87e17c788
Jens Petersen <juhpetersen@gmail.com>
2021-11-28 14:42:53 +0800

    unit6 primes: fix length typos

--- unit6/src/Primes.hs
+++ unit6/src/Primes.hs
@@ -17,20 +17,20 @@ sieve (nextPrime:rest) =
 
 isPrime :: Int -> Maybe Bool
 isPrime n | n < 2 = Nothing
-          | n >= length primes = Nothing
+          | n >= last primes = Nothing
           | otherwise = Just (n `elem` primes)
 
 unsafePrimeFactors :: Int -> [Int] -> [Int]
 unsafePrimeFactors 0 [] = []
-unsafePrimeFactors n [] = []
-unsafePrimeFactors n (next:primes) =
+unsafePrimeFactors _n [] = []
+unsafePrimeFactors n (next:primes') =
   if n `mod` next == 0
-  then next:unsafePrimeFactors (n `div` next) (next:primes)
-  else unsafePrimeFactors n primes
+  then next:unsafePrimeFactors (n `div` next) (next:primes')
+  else unsafePrimeFactors n primes'
 
 primeFactors :: Int -> Maybe [Int]
 primeFactors n | n < 2 = Nothing
-               | n >= length primes = Nothing
+               | n >= last primes = Nothing
                | otherwise = Just (unsafePrimeFactors n primesLessThanN)
   where
     primesLessThanN = filter (<= n) primes
