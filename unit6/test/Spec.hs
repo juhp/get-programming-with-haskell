9d000d0ab89c4ec39f658e387f867e83fe6bdfdf
Jens Petersen <juhpetersen@gmail.com>
2021-11-28 14:58:57 +0800

    unit6 Spec: simply prop_isPrime, prop_factorsMakeOriginal, prop_allFactorsPrime
    via hlint suggestions

--- unit6/test/Spec.hs
+++ unit6/test/Spec.hs
@@ -14,29 +14,18 @@ prop_isPrime :: Int -> Bool
 prop_isPrime val =
   case isPrime val of
     Just isprime ->
-      if isprime
-      then length divisors == 0
-      else length divisors > 0
+      isprime == null divisors
     Nothing -> True
   where
     divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
 
 prop_factorsMakeOriginal :: Int -> Bool
 prop_factorsMakeOriginal val =
-  if result == Nothing
-  then True
-  else product (fromJust result) == val
-  where
-    result = primeFactors val
+  maybe True ((== val) . product) $ primeFactors val
 
 prop_allFactorsPrime :: Int -> Bool
 prop_allFactorsPrime val =
-  if result == Nothing
-  then True
-  else all (== Just True) resultsPrime
-  where
-    result = primeFactors val
-    resultsPrime = map isPrime (fromJust result)
+  maybe True (all (== Just True) . map isPrime) $ primeFactors val
 
 main :: IO ()
 main = do
